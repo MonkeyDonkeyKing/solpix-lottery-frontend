c316fa693899dff9cf695a1d1f008fef5ce571b0
MonkeyDonkeyKing <lukas.bali15@gmail.com>
2023-10-07 15:34:17 +0200

    update ticketPage WIP

--- src/pages/tickets.tsx
+++ src/pages/tickets.tsx
@@ -1,43 +1,97 @@
 import Layout from "@/components/Layout";
 import { NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../components/TicketPage.module.css";
 import Banner from "@/components/Banner";
 import Link from "next/link";
 import { useWallet } from "@solana/wallet-adapter-react";
+import { api } from "@/utils/api";
+import { web3 } from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
 
-type Ticket = {
-  ticketID: string;
-  lotteryID: string;
-  pricePool: number;
-  priceClaimed: boolean;
-};
 
-const TestTickets: Ticket[] = [
-  {
-    ticketID: "00000001",
-    lotteryID: "001",
-    pricePool: 200,
-    priceClaimed: false,
-  },
-  {
-    ticketID: "00000002",
-    lotteryID: "001",
-    pricePool: 200,
-    priceClaimed: false,
-  },
-  {
-    ticketID: "00000003",
-    lotteryID: "001",
-    pricePool: 200,
-    priceClaimed: true,
-  },
-];
 
 const TicketPage: NextPage = () => {
-  const [tickets, setTickets] = useState<Ticket[]>(TestTickets);
   const { publicKey } = useWallet();
+  const [mintAddresses, setMintAddresses] = useState<string[]>([]);
+  const [names, setNames] = useState<string[]>([]);
+  const [lotteryIDs, setLotteryIDs] = useState<string[]>([]);
+  const [winningTickets, setWinningTickets] = useState<any[][]>([]);
+
+  
+  const userNfts = api.fetching.fetchAddressNfts.useQuery(
+   {
+    address: publicKey!.toBase58()
+   }
+   
+  );
+  const lotteryData = api.lottery.getAllLotteries.useQuery(
+    {},
+    {
+      select(data) {
+        return data.map(({ account, publicKey }) => ({
+          account: {
+            ...account,
+            associatedLotteryManager: new web3.PublicKey(
+              account.associatedLotteryManager
+            ),
+          },
+          publicKey: new web3.PublicKey(publicKey),
+        }));
+      },
+    }
+  );
+
+  const getWinningTickets = () => {
+    const winningTicketsData: { lotteryID: string; tickets: { ticketId: number, claimed: boolean }[] }[] = [];
+
+    lotteryData.data?.forEach(item => {
+      const { account } = item;
+      if (account?.lotteryId && account.winningTickets) {
+        winningTicketsData.push({
+          lotteryID: account.lotteryId.toString(),
+          tickets: account.winningTickets.map(ticketId => {
+            const claimed = winningTicketsData.find(wt => wt.lotteryID === account.lotteryId.toString())
+              ?.tickets.find(t => t.ticketId === ticketId)?.claimed;
+
+            return {
+              ticketId,
+              claimed,
+            };
+          }),
+        });
+      }
+    });
+
+    return winningTicketsData;
+  };
+
+  const winningTicketsData = getWinningTickets();
+  const filteredTickets = winningTicketsData.filter(winningTicket =>
+    mintAddresses.some(mintAddress =>
+      winningTicket.tickets.includes(mintAddress)
+    )
+  );
+
+
+  useEffect(() => {
+    if (userNfts.data) {
+      const mintAddresses = userNfts.data.map(nft => nft.mintAddress);
+      const names = userNfts.data.map(nft => nft.name);
+      setMintAddresses(mintAddresses);
+      setNames(names);
+    }
+  }, []);
+  
+  useEffect(() => {
+    if (lotteryData.data) {
+      const lotteryIDsTemp = lotteryData.data.map(item => item.account.lotteryId.toString());
+      const winningTickets = lotteryData.data.map(item => item.account.winningTickets);
+      setLotteryIDs(lotteryIDsTemp);
+      setWinningTickets(winningTickets);
+    }
+  }, []);
 
   return (
     <>
@@ -51,40 +105,53 @@ const TicketPage: NextPage = () => {
           heading="Your Tickets"
           paragraph="Your Ticket Playground: Manage, Claim, and Reap Crypto Rewards!"
         />
-        {publicKey &&
+        {publicKey && (
           <div className={styles.ticketsSection}>
             <table className={styles.ticketTable}>
               <thead>
                 <tr>
-                  <th>Ticket ID</th>
                   <th>Lottery ID</th>
-                  <th>Price Pool</th>
-                  <th>Claim Price</th>
+                  <th>Ticket ID</th>
+                  <th>Claimed</th>
                 </tr>
               </thead>
               <tbody>
-                {tickets.map((ticket) => (
-                  <tr key={ticket.ticketID}>
-                    <td>{ticket.ticketID}</td>
+                {filteredTickets.map((ticket, index) => (
+                  <tr key={index}>
                     <td>{ticket.lotteryID}</td>
-                    <td>{ticket.pricePool}</td>
-                    <td>
-                      {ticket.priceClaimed ? (
-                        <button className={styles.claimedButton} disabled>
-                          Claim Price
-                        </button>
-                      ) : (
-                        <button className={styles.claimButton}>
-                          Claim Price
-                        </button>
-                      )}
-                    </td>
+                    <td>{ticket.tickets.join(', ')}</td>
+                    <td>{ticket.tickets[index]?.claimed}</td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
-        }
+        )}
+        {publicKey && mintAddresses.length > 0 && names.length > 0 && (
+          <div className={styles.nftData}>
+            <h2>Your NFT Data</h2>
+            <ul>
+              {mintAddresses.map((address, index) => (
+                <li key={index}>
+                  Mint Address: {address}, Name: {names[index]}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        {lotteryIDs.length > 0 && winningTickets.length > 0 && (
+          <div className={styles.lotteryData}>
+            <h2>Lottery Data</h2>
+            <ul>
+              {lotteryIDs.map((key, index) => (
+                <li key={index}>
+                  Lottery ID: {key}, Winning Tickets: {winningTickets[index].join(', ')}
+                </li>
+              ))}
+            </ul>
+          </div>
+             )}
         {!publicKey && 
           <h3>Please connect your wallet first</h3>
         }
