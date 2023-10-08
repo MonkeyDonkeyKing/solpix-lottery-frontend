20853e389f50c15fe35ee9a4504874bc7ed25438
MonkeyDonkeyKing <lukas.bali15@gmail.com>
2023-10-08 19:16:54 +0200

    return html

--- src/pages/lotteries/[lottery].tsx
+++ src/pages/lotteries/[lottery].tsx
@@ -1,6 +1,8 @@
+import Layout from "@/components/Layout";
 import { api } from "@/utils/api";
 import { PublicKey } from "@solana/web3.js";
 import { NextPage } from "next";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 const LotteryDetails: NextPage = () => {
@@ -12,7 +14,21 @@ const LotteryDetails: NextPage = () => {
   const lotteryData = api.lottery.getLotteryData.useQuery({
     lottery: key.toBase58(),
   });
-  return <p>Lottery: {JSON.stringify(lotteryData.data, null, 2)}</p>;
+  return (
+    <>
+      <Head>
+        <title>Lottery Details</title>
+        <meta name="description" content="Solpix Lottery" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <div>{lotteryData.data?.lotteryId}</div>
+        <div>{lotteryData.data?.maxTicketsForSale}</div>
+        <div>{lotteryData.data?.ticketsSold}</div>
+        <div>{lotteryData.data?.ticketPrice.sol?.value}</div>
+      </Layout>
+    </>
+  );
 };
 
 export default LotteryDetails;
