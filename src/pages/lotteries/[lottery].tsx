53e11e2f975d4f3bd5924f74278a4f3848801ceb
MickSol <mikkertreserve@gmail.com>
2023-10-08 17:13:53 +0000

    improved address input handling

--- src/pages/lotteries/[lottery].tsx
+++ src/pages/lotteries/[lottery].tsx
@@ -1,9 +1,18 @@
+import { api } from "@/utils/api";
+import { PublicKey } from "@solana/web3.js";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 
 const LotteryDetails: NextPage = () => {
   const router = useRouter();
-  return <p>Post: {JSON.stringify(router.query.lottery, null, 2)}</p>;
+  const key = new PublicKey(router.query.lottery as string);
+  if (!key) return <p>Invalid solana address</p>;
+  if (PublicKey.isOnCurve(key)) return <p>Not a PDA</p>;
+
+  const lotteryData = api.lottery.getLotteryData.useQuery({
+    lottery: key.toBase58(),
+  });
+  return <p>Lottery: {JSON.stringify(lotteryData.data, null, 2)}</p>;
 };
 
 export default LotteryDetails;
